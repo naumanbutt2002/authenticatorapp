71e26bd149a234b037ec336b6481903a0773808a
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-04 10:19:18 -0400

    Create test api route

--- api/index.js
+++ api/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("Connected to MongoDB!");
-}
-).catch((error) => {
-  console.log("Error connecting to MongoDB: ", error.message);
-});
-
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error.message);
+  });
 
 const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
