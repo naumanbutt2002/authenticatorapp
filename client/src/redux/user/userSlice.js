599a4244f47f5d9151fd8417109cbc14542ff80e
naumanbutt2002 <nauman100percent@gmail.com>
2024-04-09 06:14:29 -0400

    add delete user functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -6,44 +6,59 @@ const initialState = {
   error: false,
 };
 
-const userSlice= createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-      signInStart: (state) => {
-        state.loading = true;
-      },
-      signInSuccess: (state, action) => {
-        state.currentUser = action.payload;
-        state.loading = false;
-        state.error = false;
-      },
-      signInFailure: (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      },
-      updateUserStart: (state) => {
-        state.loading = true;
-      },
-      updateUserSuccess: (state, action) => {
-        state.currentUser = action.payload;
-        state.loading = false;
-        state.error = false;
-      },
-      updateUserFailure: (state, action) => {
-        state.loading = false;
-        state.error = action.payload;
-      },
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
     },
-  });
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = false;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
 export const {
-    signInStart,
-    signInSuccess,
-    signInFailure,
-    updateUserFailure,
-    updateUserStart,
-    updateUserSuccess,
-  } = userSlice.actions;
-  
-  export default userSlice.reducer;
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateUserFailure,
+  updateUserStart,
+  updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+} = userSlice.actions;
+
+export default userSlice.reducer;
